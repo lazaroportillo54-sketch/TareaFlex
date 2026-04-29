7f63c771ca5bfe7d0af511798703e7c874b6d069
lazaroportillo54-sketch <lazaroportillo54@gmail.com>
2026-04-28 23:56:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,48 @@
-const boton = document.getElementById("btnVerificar");
+document.addEventListener("DOMContentLoaded", () => {
 
-boton.addEventListener("click", verificarEdad);
+    const boton = document.getElementById("btnVerificar");
 
-function verificarEdad() {
-    let edadInput = document.getElementById("edad");
-    let mensaje = document.getElementById("mensaje");
-    let overlay = document.getElementById("overlay");
+    boton.addEventListener("click", verificarEdad);
 
-    let edad = Number(edadInput.value);
+    function verificarEdad() {
+        let edadInput = document.getElementById("edad");
+        let mensaje = document.getElementById("mensaje");
+        let overlay = document.getElementById("overlay");
 
-    // VALIDACIONES
-    if (edadInput.value === "" || isNaN(edad)) {
-        mensaje.textContent = "Por favor ingresa una edad válida";
-        mensaje.style.color = "red";
-        return;
+        let edad = Number(edadInput.value);
+
+        // VALIDACIONES
+        if (edadInput.value === "" || isNaN(edad)) {
+            mensaje.textContent = "Por favor ingresa una edad válida";
+            mensaje.style.color = "red";
+            return;
+        }
+
+        if (edad < 0) {
+            mensaje.textContent = "No se permiten edades negativas";
+            mensaje.style.color = "red";
+            return;
+        }
+
+        if (edad < 18) {
+            mensaje.textContent = "Acceso denegado 🚫";
+            mensaje.style.color = "orange";
+            return;
+        }
+
+        // ACCESO PERMITIDO
+        mensaje.textContent = "Acceso permitido ✅";
+        mensaje.style.color = "lightgreen";
+
+        // Animación de salida
+        overlay.style.transition = "opacity 1s ease, backdrop-filter 1s ease";
+        overlay.style.opacity = "0";
+        overlay.style.backdropFilter = "blur(0px)";
+
+        // Redirección
+        setTimeout(() => {
+            window.location.href = "bienvenido.html";
+        }, 1200);
     }
 
-    if (edad < 0) {
-        mensaje.textContent = "No se permiten edades negativas";
-        mensaje.style.color = "red";
-        return;
-    }
-
-    if (edad < 18) {
-        mensaje.textContent = "Acceso denegado 🚫";
-        mensaje.style.color = "orange";
-        return;
-    }
-
-    // ACCESO PERMITIDO
-    mensaje.textContent = "Acceso permitido ✅";
-    mensaje.style.color = "lightgreen";
-
-    // Animación de salida (fade + quitar blur)
-    overlay.style.transition = "opacity 1s ease, backdrop-filter 1s ease";
-    overlay.style.opacity = "0";
-    overlay.style.backdropFilter = "blur(0px)";
-
-    // Redirección después de la animación
-    setTimeout(() => {
-        window.location.href = "bienvenido.html";
-    }, 1200);
-}
+});
