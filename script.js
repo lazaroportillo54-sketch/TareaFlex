0139a897e195873593886ee8bbab182170a4da9c
lazaroportillo54-sketch <lazaroportillo54@gmail.com>
2026-04-28 20:08:04 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const boton = document.getElementById("btnVerificar");
+
+boton.addEventListener("click", verificarEdad);
+
+function verificarEdad() {
+    let edadInput = document.getElementById("edad");
+    let mensaje = document.getElementById("mensaje");
+    let overlay = document.getElementById("overlay");
+
+    let edad = Number(edadInput.value);
+
+    // VALIDACIONES
+    if (edadInput.value === "" || isNaN(edad)) {
+        mensaje.textContent = "Por favor ingresa una edad válida";
+        mensaje.style.color = "red";
+        return;
+    }
+
+    if (edad < 0) {
+        mensaje.textContent = "No se permiten edades negativas";
+        mensaje.style.color = "red";
+        return;
+    }
+
+    if (edad < 18) {
+        mensaje.textContent = "Acceso denegado 🚫";
+        mensaje.style.color = "orange";
+        return;
+    }
+
+    // ACCESO PERMITIDO
+    mensaje.textContent = "Acceso permitido ✅";
+    mensaje.style.color = "lightgreen";
+
+    // Animación de salida (fade + quitar blur)
+    overlay.style.transition = "opacity 1s ease, backdrop-filter 1s ease";
+    overlay.style.opacity = "0";
+    overlay.style.backdropFilter = "blur(0px)";
+
+    // Redirección después de la animación
+    setTimeout(() => {
+        window.location.href = "bienvenido.html";
+    }, 1200);
+}
